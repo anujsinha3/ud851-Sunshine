41198e5e80c96b33821a838e18ce630aa147f392
Anuj Sinha <sinha.anuj0@gmail.com>
2020-07-25 22:00:11 +0530

    Adding scrollable weatherCondition TextView list on appScreen.

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,27 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // done (1) Create a field to store the weather display TextView
+    TextView mWeatherListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // done (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherListTextView = findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // done (3) Create an array of Strings that contain fake weather data
+        String[] weatherDataList = new String[]{"Sunny", "Overcast", "Rainy", "Stormy", "Partly cloudy"};
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // done (4) Append each String from the fake weather data array to the TextView
+        for(String weatherType: weatherDataList){
+            mWeatherListTextView.append(weatherType + "\n\n\n");
+        }
     }
 }
